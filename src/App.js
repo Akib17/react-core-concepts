e5ef789f7e804a14be0cc702a98a297db34ea68c
akib17 <akibahmad17@gmail.com>
2020-03-11 18:53:43 +0600

    Practice Component, State, Set state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,21 +6,46 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <Counter />
+
       </header>
     </div>
   );
 }
+
+function Counter() {
+  const [count, setCount] = useState(5)
+  return (
+    <div>
+      <h2> {count}  </h2>
+      <button onClick={() => setCount(count - 1)}>Decrease</button>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+
+      <Users />
+    </div>
+  )
+}
+
+function Users() {
+  const [users, setUsers] = useState([])
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => {
+        setUsers(data)
+      })
+  }, [])
+  return (
+    <div>
+      {
+        users.map((user) => 
+          <ol> {user.email} </ol>
+        )
+      }
+    </div>
+  )
+}
+
 
 export default App;
